ba84bc6be9d059208d77a8204cca3ff7a7079f4f
Ofir Tsabban <ofirtsabban5@gmail.com>
2023-04-23 18:04:13 +0300

    finish v1.0.1

--- Trivia/Main.cpp
+++ Trivia/Main.cpp
@@ -1,7 +1,17 @@
 #include "Server.h"
 
+#pragma comment(lib, "Ws2_32.lib")
+
 int main()
 {
+	WSADATA wsa;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return 1;
+	}
 	Server serv;
 	serv.run();
 }
