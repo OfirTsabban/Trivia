f641a57375c8f5b8f23e1cb54fbb0d19f61cff95
Itay Slavin <slavin.itay2006@gmail.com>
2023-04-27 11:46:30 +0300

    added deserializeSignupRequest

--- Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/JsonRequestPacketDeserializer.cpp
@@ -14,3 +14,19 @@ LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(char* buffer
 
 	return loginReq;
 }
+
+SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(char* buffer)
+{
+	std::string bufferString = buffer;
+	bufferString.substr(41);
+
+	auto json = nlohmann::json::parse(bufferString);
+
+	std::string username = json.at("username");
+	std::string password = json.at("password");
+	std::string email = json.at("email");
+
+	SignupRequest signupReq = { username, password, email };
+
+	return signupReq;
+}
