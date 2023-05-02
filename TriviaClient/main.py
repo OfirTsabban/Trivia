daa6fa98dfee6d7d3bb65b9a94de34f8b82c7785
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-02 15:30:52 +0300

    sent msg to server and gets response

--- TriviaClient/main.py
+++ TriviaClient/main.py
@@ -10,26 +10,25 @@ def client():
         username = input("Enter username: ")
         password = input("Enter password: ")
         json = '{username: "' + username + '", password:"' + password + '"}'
-        code_encoded = option.encode()
+        msg_code_encoded = option.encode()
 
     elif option == "2":
         username = input("Enter username: ")
         password = input("Enter password: ")
         email = input("Enter email: ")
         json = '{username: "' + username + '", password: "' + password + '", mail: "' + email + '"}'
-        code_encoded = option.encode()
+        msg_code_encoded = option.encode()
 
-    print("Json that has been created" + json)
+    print("Json that has been created: " + json)
 
-    msg_to_send = code_encoded + str(len(json)).encode() + json.encode()
+    message = msg_code_encoded + str(len(json)).encode() + json.encode()
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((socket.gethostname(), 12345))
 
-    message = "hbennn12123"
-    print("received from server: " + client_socket.recv(1024).decode())
+    client_socket.send(message)
 
-    client_socket.send(message.encode())
+    print("received from server: " + client_socket.recv(1024).decode())
 
 
 # Press the green button in the gutter to run the script.
