aa838197db6e0f226cd2486be8189254b2c29261
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-02 15:26:47 +0300

    created bytes msg to send to server

--- TriviaClient/main.py
+++ TriviaClient/main.py
@@ -2,6 +2,26 @@ import socket
 
 
 def client():
+    option = "0"
+    while option != "2" and option != "1":
+        option = input("1 - login, 2 - signup:\n")
+
+    if option == "1":
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        json = '{username: "' + username + '", password:"' + password + '"}'
+        code_encoded = option.encode()
+
+    elif option == "2":
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        email = input("Enter email: ")
+        json = '{username: "' + username + '", password: "' + password + '", mail: "' + email + '"}'
+        code_encoded = option.encode()
+
+    print("Json that has been created" + json)
+
+    msg_to_send = code_encoded + str(len(json)).encode() + json.encode()
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((socket.gethostname(), 12345))
@@ -11,39 +31,9 @@ def client():
 
     client_socket.send(message.encode())
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     client()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
