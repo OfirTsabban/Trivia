8a8294200c874ae824b37c067c65f032e9ea4742
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-03 18:24:45 +0300

    updated IRequestHandler

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -1,7 +1,23 @@
 #pragma once
+#include <vector>
+#include <ctime>
 
+struct RequestInfo
+{
+	int id;
+	std::time_t receivalTime;
+	std::vector<byte> buffer;
+};
+
+struct RequestResult
+{
+	unsigned char* response;
+	IRequestHandler* newHanlder;
+};
 
 class IRequestHandler
 {
-
+public:
+	virtual bool isRequestRelevant(RequestInfo reqInfo) = 0;
+	virtual RequestResult handleRequest(RequestInfo reqInfo) = 0;
 };
