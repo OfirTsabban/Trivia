316e066e3b9d20431be9b7e654410cfbe676b873
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-11 15:31:22 +0300

    added IDatabase pure virtual class

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+#include "sqlite3.h"
+#include "sqlite3ext.h"
+
+class IDatabase
+{
+public:
+	virtual bool doesUserExist(std::string name) = 0;
+	virtual bool doesPasswordMatch(std::string name, std::string password) = 0;
+	virtual void addNewUser(std::string name, std::string password, std::string email) = 0;
+};
