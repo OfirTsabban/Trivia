e0e7580dcc6bb16c13fa16f69eda9cf62627841e
Ofir Tsabban <ofirtsabban5@gmail.com>
2023-05-14 19:45:34 +0300

    adding classes and funcs

--- Trivia/LoginRequestHandler.h
+++ Trivia/LoginRequestHandler.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "IRequestHandler.h"
-
+#include "RequestHandlerFactory.h"
 class LoginRequestHandler : public IRequestHandler
 {
 public:
 	bool isRequestRelevant(RequestInfo reqInfo) override;
 	RequestResult handleRequest(RequestInfo reqInfo) override;
+private:
+	RequestHandlerFactory& m_handleFactory;
+	RequestResult login(RequestInfo reqInfo);
+	RequestResult signup(RequestInfo reqInfo);
 };
