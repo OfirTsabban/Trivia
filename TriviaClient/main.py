a4a0fb44b994e9bd0fb7ec9be79ec212760c9b63
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-17 17:33:37 +0300

    updated python client

--- TriviaClient/main.py
+++ TriviaClient/main.py
@@ -16,7 +16,13 @@ def client():
         username = input("Enter username: ")
         password = input("Enter password: ")
         email = input("Enter email: ")
-        json = '{username: "' + username + '", password: "' + password + '", mail: "' + email + '"}'
+        street = input("Enter street: ")
+        apt = input("Enter apartment: ")
+        city = input("Enter city: ")
+        prefix = input("Enter prefix: ")
+        number = input("Enter number: ")
+        yearBorn = input("Enter birth year: ")
+        json = '{username: "' + username + '", password: "' + password + '", mail: "' + email + '", street: "' + street + '", apt: ' + apt + ', city: "' + city + '", prefix: "' + prefix + '", number: "' + number + '", yearBorn: "' + yearBorn + '"}'
         msg_code_encoded = option.encode()
 
     print("Json that has been created: " + json)
