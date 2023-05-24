ad51f92b71386ac820528bf1bd6d6bf6a844a09f
Itay Slavin <slavin.itay2006@gmail.com>
2023-05-24 17:46:39 +0300

    updated IDatabase.h

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <iostream>
+#include <list>
+#include <vector>
 #include "sqlite3.h"
+#include "Question.h"
 
 class IDatabase
 {
@@ -8,5 +11,11 @@ public:
 	virtual bool doesUserExist(std::string name) = 0;
 	virtual bool doesPasswordMatch(std::string name, std::string password) = 0;
 	virtual void addNewUser(std::string name, std::string password, std::string email, std::string street, std::string apt, std::string city, std::string prefix, std::string number, std::string yearBorn) = 0;
-
+	virtual std::list<Question> getQuestions(int num) = 0;
+	virtual float getPlayerAverageAnswerTime(std::string name) = 0;
+	virtual int getNumOfCorrectAnswers(std::string name) = 0;
+	virtual int getNumOfTotalAnswers(std::string name) = 0;
+	virtual int getNumOfPlayerGames(std::string name) = 0;
+	virtual int getPlayerScore(std::string name) = 0;
+	virtual std::vector<std::string> getHighScores() = 0;
 };
