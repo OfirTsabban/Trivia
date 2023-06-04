3bcbc7d468801ef4d9f93540559a39a9c8e62783
Itay Slavin <slavin.itay2006@gmail.com>
2023-06-04 15:44:22 +0300

    added Connector class and his static builder

---
+++ GUI/Connector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal class Connector
+    {
+        public static bool connected { get; set; }
+
+        static Connector()
+        {
+            TcpClient client = new TcpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            try
+            {
+                client.Connect(serverEndPoint);
+            }
+            catch
+            {
+                connected = false;
+                return;
+            }
+
+            connected = true;
+        }
+    }
+}
