a990c760bde321d695788c5b2c24696ff2d48cbd
Itay Slavin <slavin.itay2006@gmail.com>
2023-06-17 21:52:17 +0300

    added Game.cpp

---
+++ Trivia/Game.cpp
@@ -0,0 +1,53 @@
+#include "Game.h"
+
+Game::Game(std::vector<Question> questions, std::vector<LoggedUser> players, unsigned int gameId)
+{
+	std::unique_lock<std::mutex> lock(this->gameMutex);
+	for (auto& question : questions)
+	{
+		this->m_questions.push_back(question);
+	}
+	for (LoggedUser user : players)
+	{
+		GameData gameData = { questions[0], 0, 0, 0 };
+		this->m_players.emplace(std::make_pair(user, gameData));
+	}
+
+	this->m_gameId = gameId;
+}
+
+Question Game::getQuestionForUser(LoggedUser user)
+{
+	std::unique_lock<std::mutex> lock(this->gameMutex);
+
+	for (auto it = this->m_players.begin(); it != this->m_players.end(); it++)
+	{
+		LoggedUser currUser = it->first;
+		if (currUser == user)
+		{
+			return it->second.currentQuestion;
+		}
+	}
+
+	throw("User wasn't found");
+}
+
+void Game::submitAnswer(LoggedUser user, std::string answer)
+{
+	auto it = m_players.find(user);
+	
+	if (it->second.currentQuestion.getRightAnswer() == answer)
+	{
+		it->second.correctAnswerCount++;
+	}
+	else
+	{
+		it->second.wrongAnswerCount++;
+	}
+}
+
+void Game::removePlayer(LoggedUser user)
+{
+	auto it = m_players.find(user);
+	m_players.erase(it);
+}
--- Trivia/Game.h
+++ Trivia/Game.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <vector>
 #include <map>
+#include <mutex>
 #include "Question.h"
 #include "LoggedUser.h"
+
+using std::mutex;
 
 typedef struct GameData
 {
@@ -15,12 +18,14 @@ typedef struct GameData
 class Game
 {
 public:
+	Game(std::vector<Question> questions, std::vector<LoggedUser> players, unsigned int gameId);
 	Question getQuestionForUser(LoggedUser);
-	void submitAnswer(LoggedUser, unsigned int id);
+	void submitAnswer(LoggedUser, std::string answer);
 	void removePlayer(LoggedUser);
 
 private:
 	std::vector<Question> m_questions;
 	std::map<LoggedUser, GameData> m_players;
 	unsigned int m_gameId;
+	mutex gameMutex;
 };
